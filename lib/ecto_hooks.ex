610af516722e18f4d55454255bbe26af5f4c3e6f
Chris Bailey <me@cbailey.co.uk>
2020-12-15 12:30:15 +0000

    Add after_insert hook support for Ecto.Repo.insert/2 and Ecto.Repo.insert!/2

--- lib/ecto_hooks.ex
+++ lib/ecto_hooks.ex
@@ -32,5 +32,48 @@ defmodule Ecto.Repo.Hooks do
   """
 
   defmacro __using__(_opts) do
+    quote do
+      @hooks unquote(__MODULE__)
+
+      defoverridable all: 2,
+                     delete: 2,
+                     delete!: 2,
+                     get: 3,
+                     get!: 3,
+                     get_by: 3,
+                     get_by!: 3,
+                     insert: 2,
+                     insert!: 2,
+                     insert_or_update: 2,
+                     insert_or_update!: 2,
+                     one: 2,
+                     one!: 2,
+                     update: 2,
+                     update!: 2
+
+      def insert(query, opts) do
+        with {:ok, result} <- super(query, opts) do
+          {:ok, @hooks.after_insert(result)}
+        end
+      end
+
+      def insert!(query, opts) do
+        query
+        |> super(opts)
+        |> @hooks.after_insert
+      end
+    end
+  end
+
+  def after_insert(%schema{} = data) do
+    if function_exported?(schema, :after_insert, 1) do
+      schema.after_insert(data)
+    else
+      data
+    end
+  end
+
+  def after_insert(data) do
+    data
   end
 end
--- test/ecto_hooks_test.exs
+++ test/ecto_hooks_test.exs
@@ -1,15 +1,17 @@
 defmodule Ecto.Repo.HooksTest do
   defmodule Repo do
-    use Ecto.Repo.Hooks
-
     use Ecto.Repo,
       otp_app: :ecto_hooks,
       adapter: Etso.Adapter
+
+    use Ecto.Repo.Hooks
   end
 
   defmodule User do
     use Ecto.Schema
     import Ecto.Changeset
+
+    require Logger
 
     schema "user" do
       field(:first_name, :string)
@@ -18,18 +20,65 @@ defmodule Ecto.Repo.HooksTest do
       field(:full_name, :string, virtual: true)
     end
 
+    def after_insert(%__MODULE__{first_name: first_name, last_name: last_name} = data) do
+      Logger.info("after insert")
+      %__MODULE__{data | full_name: first_name <> " " <> last_name}
+    end
+
     def changeset(%__MODULE__{} = user, attrs) do
       user
       |> cast(attrs, [:first_name, :last_name])
+      |> validate_required([:first_name, :last_name])
     end
   end
 
   use ExUnit.Case
+  import ExUnit.CaptureLog
 
   setup do
-    repo_start = {Repo, :start_link, []}
     {:ok, _} = start_supervised(%{id: __MODULE__, start: {Repo, :start_link, []}})
-
     :ok
+  end
+
+  describe "after_insert/1" do
+    test "executes after successful Repo.insert/2" do
+      assert capture_log(fn ->
+               assert {:ok, user} =
+                        %User{}
+                        |> User.changeset(%{first_name: "Bob", last_name: "Dylan"})
+                        |> Repo.insert()
+
+               assert user.full_name == "Bob Dylan"
+             end) =~ "after insert"
+    end
+
+    test "executes after successful Repo.insert!/2" do
+      assert capture_log(fn ->
+               assert user =
+                        %User{}
+                        |> User.changeset(%{first_name: "Bob", last_name: "Dylan"})
+                        |> Repo.insert!()
+
+               assert user.full_name == "Bob Dylan"
+             end) =~ "after insert"
+    end
+
+    test "does not executes after unsuccessful Repo.insert/2" do
+      refute capture_log(fn ->
+               assert {:error, %Ecto.Changeset{}} =
+                        %User{}
+                        |> User.changeset(%{})
+                        |> Repo.insert()
+             end) =~ "after insert"
+    end
+
+    test "does not executes after unsuccessful Repo.insert!/2" do
+      assert_raise Ecto.InvalidChangesetError, fn ->
+        assert %Ecto.Changeset{} =
+                 %User{}
+                 |> User.changeset(%{})
+                 |> Repo.insert!()
+      end
+    end
   end
 end
