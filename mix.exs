96d615a2ff4dc5e995c2552583c758497521b20a
Chris Bailey <me@cbailey.co.uk>
2022-03-21 11:56:42 +0000

    Bump patch version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule EctoHooks.MixProject do
     [
       aliases: aliases(),
       app: :ecto_hooks,
-      version: "1.0.0",
+      version: "1.0.1",
       elixir: "~> 1.10",
       elixirrc_options: [warnings_as_errors: true],
       start_permanent: Mix.env() == :prod,
