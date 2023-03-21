669d6fcd5ea4fd741bae948b0f2d2d53ccfe4e19
Tomasz Tomczyk <me@tomasztomczyk.com>
2023-03-21 22:35:50 +0000

    Ensure we allow enable/disable hooks globally from the public API

--- test/ecto_hooks/state_test.exs
+++ test/ecto_hooks/state_test.exs
@@ -3,45 +3,45 @@ defmodule EctoHooks.StateTest do
 
   alias EctoHooks.State
 
-  describe "disable_hooks/0" do
+  describe "disable_hooks/1" do
     test "when already enabled, disables hooks such that `hooks_enabled?/0` returns false" do
       assert State.hooks_enabled?()
-      assert :ok = State.disable_hooks()
+      assert :ok = State.disable_hooks(global: false)
       refute State.hooks_enabled?()
     end
 
     test "disabling hooks multiple times is a noop" do
       assert State.hooks_enabled?()
-      assert :ok = State.disable_hooks()
-      assert :ok = State.disable_hooks()
+      assert :ok = State.disable_hooks(global: false)
+      assert :ok = State.disable_hooks(global: false)
       refute State.hooks_enabled?()
     end
   end
 
-  describe "enable_hooks/0" do
+  describe "enable_hooks/1" do
     test "when already enabled, disables hooks such that `hooks_enabled?/0` returns false" do
-      assert :ok = State.disable_hooks()
+      assert :ok = State.disable_hooks(global: false)
       refute State.hooks_enabled?()
-      assert :ok = State.enable_hooks()
+      assert :ok = State.enable_hooks(global: false)
       assert State.hooks_enabled?()
     end
 
     test "enabling hooks multiple times is a noop" do
       assert State.hooks_enabled?()
-      assert :ok = State.enable_hooks()
-      assert :ok = State.enable_hooks()
+      assert :ok = State.enable_hooks(global: false)
+      assert :ok = State.enable_hooks(global: false)
       assert State.hooks_enabled?()
     end
   end
 
   describe "hooks_enabled?/0" do
     test "returns true if hooks enabled" do
-      assert :ok = State.enable_hooks()
+      assert :ok = State.enable_hooks(global: false)
       assert State.hooks_enabled?()
     end
 
     test "returns false if hooks disabled" do
-      assert :ok = State.disable_hooks()
+      assert :ok = State.disable_hooks(global: false)
       refute State.hooks_enabled?()
     end
   end
